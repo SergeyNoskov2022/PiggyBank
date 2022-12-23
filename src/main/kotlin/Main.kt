ec28ca08d66936c3978fa257c8e32f792b41e15c
SergeyNoskov2022 <wraith10@yandex.ru>
2022-12-23 12:52:30 +0500

    comm1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,23 +1,30 @@
 object PiggyBank {
 
-    private var money: ArrayList// список монеток/купюр
+    private var moneys = ArrayList<Money>() // список монеток/купюр
     private var isBroken: Boolean = false // свойство, определяющее, разбита ли копилка
 
     fun putMoney(money: Money) {
-        if (isBroken) { // проверьте, не разбита ли копилка
-
-        ...
-        }// добавьте монетку в копилку
+        if (isBroken) println("Вы разбили копилку, вы больше ничего положить туда не можете") // проверьте, не разбита ли копилка
+            else {
+                moneys.add(money)
+                println("Добавлено в копилку $money")
+        }  // добавьте монетку в копилку
     }
 
     fun shake(): Money? {
-        ... // проверьте, не разбита ли копилка
-
-        ... // вытрясти монетку из копилки (если в копилке нет монетки, вернуть null). Помните, купюры вытрясти нельзя.
+        if (isBroken) { // проверьте, не разбита ли копилка
+            val moneyIterator = moneys.iterator()
+            if (moneyIterator.next().isCoin) {
+                moneyIterator.remove()
+            } else return null // вытрясти монетку из копилки (если в копилке нет монетки, вернуть null). Помните, купюры вытрясти нельзя.
+        } else println("Вы разбили копилку, больше оттуда ничего не вытрясти")
+        return null
     }
 
     fun smash(): ArrayList<Money> {
-        ... // установить флаг, что копилка разбита true, и вернуть все монетки, которые были в копилке
+        println("Копилка разбита, вы достали оттуда: $moneys")
+        isBroken = true
+        return moneys      // установить флаг, что копилка разбита true, и вернуть все монетки, которые были в копилке
     }
 }
 
@@ -26,17 +33,17 @@ class Money private constructor(
     val isCoin: Boolean
 ){
     companion object {
-        private val coins_10 = Money(0.1f, true)
-        private val coins_50 = Money(0.5f, true)
-        private val coins_100 = Money(1f, true)
-        private val bill_50 = Money(50f, false)
-        private val bill_100 = Money(100f, false)
-        private val bill_500 = Money(500f, false)
-        private val bill_1000 = Money(1000f, false)
+        val coins_10 = Money(0.1f, true)
+        val coins_50 = Money(0.5f, true)
+        val coins_100 = Money(1f, true)
+        val bill_50 = Money(50f, false)
+        val bill_100 = Money(100f, false)
+        val bill_500 = Money(500f, false)
+        val bill_1000 = Money(1000f, false)
     }
 
     override fun toString(): String {
-        if (amount < 1) return "${(amount*100).toInt()} коп." else  return "${amount.toInt()} руб."
+        if (amount < 1) return "${(amount*100).toInt()} коп." else return "${amount.toInt()} руб."
     }
 }
 
