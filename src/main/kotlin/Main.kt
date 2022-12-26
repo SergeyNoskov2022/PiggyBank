32d3ce9af7838ea6a8ecca949b7e2c51a2453005
SergeyNoskov2022 <wraith10@yandex.ru>
2022-12-26 21:13:02 +0500

    comm7

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,6 +3,7 @@ object PiggyBank {
     private var moneys = ArrayList<Money>() // список монеток/купюр
     private var isBroken: Boolean = false // свойство, определяющее, разбита ли копилка
 
+
     fun putMoney(money: Money) {
         if (isBroken) println("Вы разбили копилку, вы больше ничего положить туда не можете") // проверьте, не разбита ли копилка
         else {
@@ -11,33 +12,38 @@ object PiggyBank {
         }  // добавьте монетку в копилку
     }
 
-    fun shake(): Money? {
-        if (isBroken) {
-            val iterator = moneys.iterator()
-            while (iterator.hasNext()) {
-                val coin = iterator.next()
-                if (coin.isCoin) {
-                    moneys.remove(coin)
-                    return coin
-                }
-            }
-            return null // вытрясти монетку из копилки (если в копилке нет монетки, вернуть null). Помните, купюры вытрясти нельзя.
-        } else {
-            return null
+    fun shake(): Money?  {
+        // проверьте, не разбита ли копилка
+        if (isBroken){
             println("Вы разбили копилку, больше оттуда ничего не вытрясти")
+            return null
         }
+        val iterator = moneys.iterator()
+        while(iterator.hasNext()) {
+            var coin = iterator.next()
+            if (coin.isCoin){
+                moneys.remove(coin)//удалаем монетку из копилки
+                return coin
+            }
+        }
+        return null//если в копилке нет монеток возвращаем null
     }
+
+
 
     fun smash(): ArrayList<Money> {
         isBroken = true
         println("Копилка разбита, вы достали оттуда: $moneys")
         return moneys      // установить флаг, что копилка разбита true, и вернуть все монетки, которые были в копилке
     }
+
 }
 
-class Money private constructor(
-    val amount: Float,
-    val isCoin: Boolean
+// создайте класс Money, который будет отражать сущность монетки/купюры с двумя полями: amount и isCoin
+
+class Money  private constructor (
+    var amount:Float,
+    var isCoin: Boolean
 ){
     companion object {
         val coins_10 = Money(0.1f, true)
@@ -49,11 +55,9 @@ class Money private constructor(
         val bill_1000 = Money(1000f, false)
     }
 
+    // переопределите метод toString() так, чтобы он возвращал строку типа "10 коп.", если это монетка, и "100 руб.", если это купюра
     override fun toString(): String {
         if (amount < 1) return "${(amount*100).toInt()} коп." else return "${amount.toInt()} руб."
     }
-}
 
-// создайте класс Money, который будет отражать сущность монетки/купюры с двумя полями: amount и isCoin
-// переопределите метод toString() так, чтобы он возвращал строку типа "10 коп." или "1 руб.", если это монетка, и "100 руб.", если это купюра
-// вы должны ограничить создание класса таким образом, чтобы можно было создать только ограниченный набор номиналов (см. задание)
+}
